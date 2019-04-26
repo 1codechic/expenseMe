ad3a1eb4e16c1f71efa9ea3bd4c8e48ef03626a0
1codechic <1codechic@devchics.com>
2019-04-26 15:00:55 -0400

    configured jwt_sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
-  # protect_from_forgery with: :null_session
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+  protect_from_forgery with: :null_session
+
+   private
+
+    def current_user
+    @current_user ||= User.find(payload['user_id'])
+    end
+
+    def not_authorized
+     render json: { error: "Not authorized" }, status: :unauthorized
+    end
 end
